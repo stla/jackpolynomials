5ca45aa55d7ff6fc9be29e1a732b81ccdaeb3789
stla <laurent_step@yahoo.fr>
2024-04-03 08:07:30 +0200

    started MSP stuff

--- src/Math/Algebra/Jack/Auxiliary.hs
+++ src/Math/Algebra/Jack/Auxiliary.hs
@@ -1,9 +1,11 @@
 module Math.Algebra.Jack.Auxiliary
   ( msPolynomial
   , msCombination
+  , prettySymmetricSpray
   ) where
 import qualified Algebra.Ring                     as AlgRing
-import           Data.List                        ( nub )
+import           Data.Function                    ( on )
+import           Data.List                        ( nub, sortBy )
 import           Data.Map.Strict                  ( Map )
 import qualified Data.Map.Strict                  as DM
 import           Math.Algebra.Hspray              (
@@ -19,11 +21,19 @@ import           Math.Algebra.Hspray              (
 import           Math.Algebra.Jack.Internal       ( Partition , _isPartition )
 import           Math.Combinat.Permutations       ( permuteMultiset )
 import           Math.Combinat.Partitions.Integer ( fromPartition, mkPartition )
+import           Data.Text                        ( Text
+                                                  , append
+                                                  , cons
+                                                  , intercalate
+                                                  , pack
+                                                  , snoc
+                                                  , unpack
+                                                  )
 
 -- | Monomial symmetric polynomials
 --
 -- >>> putStrLn $ prettySpray' (msPolynomial 3 [2, 1])
--- (1) x1^2x2 + (1) x1^2x3 + ......
+-- (1) x1^2x2 + (1) x1^2x3 + ...... TODO
 msPolynomial 
   :: (AlgRing.C a, Eq a) 
   => Int       -- ^ number of variables
@@ -39,13 +49,34 @@ msPolynomial n lambda
       permutations = permuteMultiset (lambda ++ replicate (n-llambda) 0)
       coefficients = repeat AlgRing.one
 
+-- | Symmetric polynomial as a linear combination of monomial symmetric polynomials
 msCombination 
   :: AlgRing.C a
   => Spray a
   -> Map Partition a
-msCombination spray = DM.fromList assocs
-  where
-    -- constantTerm = constantSpray (getConstantTerm spray) -- useless ?
-    lambdas = nub $ map (fromPartition . mkPartition . fst) (toList spray)
-    assocs  = map (\lambda -> (lambda, getCoefficient lambda spray)) lambdas
+msCombination spray = DM.fromList (msCombination' spray)
 
+msCombination' :: AlgRing.C a => Spray a -> [(Partition, a)]
+msCombination' spray = 
+  map (\lambda -> (lambda, getCoefficient lambda spray)) lambdas
+  where
+    lambdas = nub $ map (fromPartition . mkPartition . fst) (toList spray)
+    -- constantTerm = constantSpray (getConstantTerm spray) -- useless ?
+
+-- | prettyMonomial [0, 2, 1] = M[0, 2, 1]
+prettyMonomial :: Partition -> Text
+prettyMonomial lambda = append (pack "M") (cons '[' $ snoc string ']')
+  where
+    string = intercalate (pack ", ") (map (pack . show) lambda)
+
+prettySymmetricSpray :: (Show a, AlgRing.C a) => Spray a -> String
+prettySymmetricSpray spray = unpack $ intercalate (pack " + ") termsText
+ where
+  assocs = msCombination' spray
+  termsText     = 
+    map termText (sortBy (flip compare `on` fst) assocs)
+  termText assoc = append
+    (snoc (snoc (cons '(' $ snoc coefText ')') ' ') '*')
+    (prettyMonomial $ fst assoc)
+   where
+    coefText = pack $ show (snd assoc)
