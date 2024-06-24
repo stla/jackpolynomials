0f9ed702d589d7b70fc4298e05f38f778dfd8198
stla <laurent_step@yahoo.fr>
2024-06-25 00:48:55 +0200

    started skewJack

--- src/Math/Algebra/SymmetricPolynomials.hs
+++ src/Math/Algebra/SymmetricPolynomials.hs
@@ -541,39 +541,55 @@ psCombination'' ::
 psCombination'' = 
   _psCombination (\coef r -> fromRational r *^ coef)
 
+-- _hallInnerProductFromPScombinations :: 
+--   forall b. (Eq b, AlgRing.C b)
+--   => (Spray b -> Spray b -> Spray b)
+--   -> Map Partition (Spray b)
+--   -> Map Partition (Spray b)
+--   -> Spray b         -- ^ parameter
+--   -> Spray b 
+-- _hallInnerProductFromPScombinations multabFunc psCombo1 psCombo2 alpha = 
+--   AlgAdd.sum $ DM.elems
+--     (merge dropMissing dropMissing (zipWithMatched f) psCombo1 psCombo2)
+--   where
+--     zlambda' :: Partition -> Spray b
+--     zlambda' lambda = fromIntegral (zlambda lambda) 
+--       AlgRing.* alpha AlgRing.^ (toInteger $ length lambda)
+--     f :: Partition -> Spray b -> Spray b -> Spray b
+--     f lambda coeff1 coeff2 = 
+--       multabFunc (zlambda' lambda) (coeff1 AlgRing.* coeff2)
+
 _hallInnerProductFromPScombinations :: 
   forall b. (Eq b, AlgRing.C b)
-  => (Spray b -> Spray b -> Spray b)
-  -> Map Partition (Spray b)
-  -> Map Partition (Spray b)
-  -> Spray b         -- ^ parameter
+  => Map Partition b
+  -> Map Partition b
   -> Spray b 
-_hallInnerProductFromPScombinations multabFunc psCombo1 psCombo2 alpha = 
+_hallInnerProductFromPScombinations psCombo1 psCombo2 = 
   AlgAdd.sum $ DM.elems
     (merge dropMissing dropMissing (zipWithMatched f) psCombo1 psCombo2)
   where
+    alpha = lone 1 :: Spray b
     zlambda' :: Partition -> Spray b
-    zlambda' lambda = fromIntegral (zlambda lambda) 
-      AlgRing.* alpha AlgRing.^ (toInteger $ length lambda)
-    f :: Partition -> Spray b -> Spray b -> Spray b
+    zlambda' lambda = zlambda lambda .^ (alpha ^**^ (length lambda))
+    f :: Partition -> b -> b -> Spray b
     f lambda coeff1 coeff2 = 
-      multabFunc (zlambda' lambda) (coeff1 AlgRing.* coeff2)
+      (coeff1 AlgRing.* coeff2) *^ (zlambda' lambda) 
 
-symbolicHallInnerProductFromPScombinations :: 
-  (Eq b, AlgField.C b) => Map Partition b -> Map Partition b -> Spray b
-symbolicHallInnerProductFromPScombinations =
-  _symbolicHallInnerProductFromPScombinations 
-    (
-      _hallInnerProductFromPScombinations (^*^)
-    ) 
-_symbolicHallInnerProductFromPScombinations :: 
-  (Eq b, AlgRing.C b) 
-  => (Map Partition (Spray b) -> Map Partition (Spray b) -> Spray b -> Spray b) 
-  -> Map Partition b -> Map Partition b -> Spray b
-_symbolicHallInnerProductFromPScombinations func psCombo1 psCombo2 = func psCombo1' psCombo2' (lone 1)
-  where
-    psCombo1' = DM.map constantSpray psCombo1
-    psCombo2' = DM.map constantSpray psCombo2
+-- symbolicHallInnerProductFromPScombinations :: 
+--   (Eq b, AlgField.C b) => Map Partition b -> Map Partition b -> Spray b
+-- symbolicHallInnerProductFromPScombinations =
+--   _symbolicHallInnerProductFromPScombinations 
+--     (
+--       _hallInnerProductFromPScombinations (^*^)
+--     ) 
+-- _symbolicHallInnerProductFromPScombinations :: 
+--   (Eq b, AlgRing.C b) 
+--   => (Map Partition (Spray b) -> Map Partition (Spray b) -> Spray b -> Spray b) 
+--   -> Map Partition b -> Map Partition b -> Spray b
+-- _symbolicHallInnerProductFromPScombinations func psCombo1 psCombo2 = func psCombo1' psCombo2' (lone 1)
+--   where
+--     psCombo1' = DM.map constantSpray psCombo1
+--     psCombo2' = DM.map constantSpray psCombo2
 
 jackInPSbasis ::
   forall a. (Eq a, AlgField.C a) => Char -> Partition -> Map Partition (RatioOfSprays a)
@@ -584,21 +600,21 @@ jackInPSbasis which mu =
     jackCombo = jackInMSPbasis which mu :: Map Partition (RatioOfSprays a)
     combo_to_map lambda rOS = 
       DM.map 
-        (\r -> (fromRational r :: a) AlgMod.*> rOS) 
-          (psCombination (msPolynomial (sum lambda) lambda))
+        (\r -> (fromRational r :: a) AlgMod.*> rOS) (mspInPSbasis lambda)
+--          (psCombination (msPolynomial (sum lambda) lambda))
 
 skewJackPolTerm :: 
   forall a. (Eq a, AlgField.C a) => Int -> Char -> Partition -> Partition -> Partition -> ParametricSpray a
 skewJackPolTerm n which lambda mu nu = 
-  constantRatioOfSprays (evaluate coeff [AlgRing.one]) *^ jackSymbolicPol n nu which
+  coeff *^ jackSymbolicPol n nu which
   where
     psCombo_lambda = jackInPSbasis which lambda
     psCombo_mu = jackInPSbasis which mu
     psCombo_nu = jackInPSbasis which nu
     psCombo_mu_nu = psCombinationsProduct psCombo_mu psCombo_nu
     coeff = 
-      evaluate (symbolicHallInnerProductFromPScombinations psCombo_lambda psCombo_mu_nu) [lone 1 %//% unitSpray] AlgField./ 
-        evaluate (symbolicHallInnerProductFromPScombinations psCombo_nu psCombo_nu) [lone 1 %//% unitSpray]
+      evaluate (_hallInnerProductFromPScombinations psCombo_lambda psCombo_mu_nu) [lone 1 %//% unitSpray] AlgField./ 
+        evaluate (_hallInnerProductFromPScombinations psCombo_nu psCombo_nu) [lone 1 %//% unitSpray]
     -- hip = _hallInnerProductFromPScombinations (AlgRing.*) :: 
     --   Map Partition (ParametricSpray a) -> Map Partition (ParametricSpray a) -> ParametricSpray a -> ParametricSpray a
     -- alpha = lone 1 :: ParametricSpray a
